05f4ef3d60268e0b830205281bfe1bd07a6f0e93
miljkom <miljkom99@gmail.com>
2022-09-08 20:03:16 +0200

    Wall Jumping
    Wall jumping, Wall Sliding

--- Assets/Scripts/Controller2D.cs
+++ Assets/Scripts/Controller2D.cs
@@ -11,6 +11,7 @@ public class Controller2D : RaycastController
 
 	public override void Start() {
 		base.Start();
+		collisions.faceDir = 1;
 	}
 	public void Move(Vector3 velocity, bool standingOnPlatform = false)
 	{
@@ -18,14 +19,18 @@ public class Controller2D : RaycastController
 		collisions.Reset();
 		collisions.velocityOld = velocity;
 
+		if(velocity.x != 0)
+        {
+			collisions.faceDir = (int)Mathf.Sign(velocity.x);
+        }
+
 		if (velocity.y < 0)
 		{
 			DescendSlope(ref velocity);
 		}
-		if (velocity.x != 0)
-		{
-			HorizontalCollisions(ref velocity);
-		}
+
+		HorizontalCollisions(ref velocity);
+
 		if (velocity.y != 0)
 		{
 			VerticalCollisions(ref velocity);
@@ -41,8 +46,13 @@ public class Controller2D : RaycastController
 
 	void HorizontalCollisions(ref Vector3 velocity)
 	{
-		float directionX = Mathf.Sign(velocity.x);
+		float directionX = collisions.faceDir;
 		float rayLength = Mathf.Abs(velocity.x) + skinWidth;
+
+		if(Mathf.Abs(velocity.x) < skinWidth)
+        {
+			rayLength = 2 * skinWidth;
+        }
 
 		for (int i = 0; i < horizontalRayCount; i++)
 		{
@@ -194,6 +204,7 @@ public class Controller2D : RaycastController
 		public bool descendingSlope;
 		public float slopeAngle, slopeAngleOld;
 		public Vector3 velocityOld;
+		public int faceDir;
 
 		public void Reset()
 		{
